50ddc882207fdd290bceadc00d204aec045177b4
Siddharth Ray <sidraystudios@gmail.com>
2023-07-18 22:24:51 -0700

    inited apriltags in robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.subsystems.vision.AprilTags;
+
 public class Robot {
+    //SUBSYSTEMS
+    AprilTags aprilTags;
+
+
+    public Robot(HardwareMap map) {
+        aprilTags = new AprilTags(map.get(WebcamName.class, "Webcam 1"));
+    }
 }
